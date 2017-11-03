acddb870d15909edaaff7d4b0bd12d3a7a628a5d
kjabr <kjabr@users.noreply.github.com>
2017-11-03 07:57:45 -0700

    Add files via upload

--- number_search_puzzle.py
+++ number_search_puzzle.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 import random
-
+import sys
+import time
 
 def overlap(start1, end1, start2, end2):
     # using this routine to check if two lines overlap. Essential if they
@@ -95,159 +96,251 @@ numbers_puzz=""
 position_from_top = 80 # for display in the html file.
 regen_coord = 1
 
+number_of_puzzles = 1
+
+if len(sys.argv) > 1:
+    number_of_puzzles = sys.argv[1]
+
 # the html_content string
 html_content = """<HTML>
 <!doctype html public "-//w3c//dtd html 3.2//en">
 
 <head>
+<style>
+
+.container {
+    width: 800px;
+    position: relative;
+}
+
+.left-element {
+    display: inline-block;
+    position: relative;
+    outline: #2eb7ed solid medium;
+    padding: 10px;
+    font-family: arial;
+    font-weight: bold;
+    font-size: 25;
+    letter-spacing: 4px;
+    left: 20;
+    top: 40;
+    margin-left: 20px;
+}
+
+.left_under {
+    display: inline-block;
+    position: absolute;
+    font-family: arial;
+    font-weight: bold;
+    padding-top: 570px;
+    vertical-align: middle;
+    font-size: 15;
+    left: 230;
+
+}
+
+.right-element {
+    display: inline-block;
+    position: absolute;
+    font-family: arial;
+    font-weight: bold;
+    font-size: 25;
+    letter-spacing: 4px;
+    right: 0;
+}
+
+
+.pagebreak { page-break-after: always;
+    position: relative; }
+</style>
 </head>
 
-<body bgcolor="ffffff" text="000000" link="0000ff" vlink="800080" alink="ff0000">
+<body bgcolor="ffffff" text="000000" link="0000ff" vlink="800080" alink="ff0000"> """
 
-"""
+for repeat in range (int(number_of_puzzles)):
 
-# Need to produce the number matrix. Below implemented as 17x17
-for y in range (17):
-    row=[]
-    for x in range (17):
-        row.append(random.randint(0,9))
-    matrix.append(row)
+    # Need to produce the number matrix. Below implemented as 17x17
+    for y in range (17):
+        row=[]
+        for x in range (17):
+            row.append(random.randint(0,9))
+        matrix.append(row)
 
-# Next part takes each line from them matrix, saves to a variable and then adds to the html
-# file using the variable html_content. This probably can be combined with the loop that produced
-# the matrix list. But decided to keep it separate to keep it easier to understand
+    # Next part takes each line from them matrix, saves to a variable and then adds to the html
+    # file using the variable html_content. This probably can be combined with the loop that produced
+    # the matrix list. But decided to keep it separate to keep it easier to understand
 
-for n in range (17):
-    matrix_puzz = " ".join([str(x) for x in matrix[n]])
-    html_content += """<DIV style="position: absolute; font-family: arial; font-style: italic;
-        font-variant:small-caps; font-weight:bold;letter-spacing: 4px; font-size:25;
-        top: """ + str(position_from_top) + """px; width: 550px; left: 40px; height: 25px">"""
-    html_content += matrix_puzz
-    html_content += """</div>
+    html_content += """<div class="container">
+        <div class="left-element">
+
     """
-    position_from_top += 29
 
-# Next part creates multiple random coordinates to look for the numbers in the puzzle
+    for n in range (17):
+        matrix_puzz = " ".join([str(x) for x in matrix[n]])
+        #html_content += """<div1>"""
+        html_content += matrix_puzz
+        html_content += """<br>"""
 
-position_from_top = 40  # need to reinit the position from top for the numbers
-                        # displayed on the right side of the puzzle
+    html_content += """</div>"""
 
-# The next part is generating 21 numbers. ie numbers to search.
+    html_content += """<div class="left_under">"""
+    html_content += """Puzzle Number: """
+    html_content += str(repeat+1)
+    html_content += """</div>"""
 
-for p in range (21):
-    row, column, func, length, direction = coord()
-    while (regen_coord):
+        #position_from_top += 29
 
-# Need to look for overlapping numbers in the puzzle. Lists hd1, hd2,
-# vd1, vd2, and dd1-4 act as history buffer to compare against
+    # Next part creates multiple random coordinates to look for the numbers in the puzzle
 
-        if (func == 1 and direction == 1):  #right
-            if hd1 and not dup_list:
-                dup_list = [items for items in hd1 if
-                    ((overlap (items[1], (items[1]+items[2]), column,
-                    (column+length)) and (items[0] == row)))]
+    html_content += """<div class="right-element">
+    """
 
-            if hd2 and not dup_list:
-                dup_list = [items for items in hd2 if
-                    ((overlap ((items[1]-items[2]), items[1], column,
+    # pos_from_top_numbers = 80  # need to reinit the position from top for the numbers
+                            # displayed on the right side of the puzzle
+
+    # The next part is generating 21 numbers. ie numbers to search.
+    for p in range (21):
+        row, column, func, length, direction = coord()
+        while (regen_coord):
+
+    # Need to look for overlapping numbers in the puzzle. Lists hd1, hd2,
+    # vd1, vd2, and dd1-4 act as history buffer to compare against
+
+            if (func == 1 and direction == 1):  #right
+                if hd1 and not dup_list:
+                    dup_list = [items for items in hd1 if
+                        ((overlap (items[1], (items[1]+items[2]), column,
                         (column+length)) and (items[0] == row)))]
 
-        elif (func == 1 and direction == 2):  # left
-            if hd2 and not dup_list:
-                dup_list = [items for items in hd2 if
-                    ((overlap ((items[1]-items[2]), items[1], (column-length),
+                if hd2 and not dup_list:
+                    dup_list = [items for items in hd2 if
+                        ((overlap ((items[1]-items[2]), items[1], column,
+                            (column+length)) and (items[0] == row)))]
+
+            elif (func == 1 and direction == 2):  # left
+                if hd2 and not dup_list:
+                    dup_list = [items for items in hd2 if
+                        ((overlap ((items[1]-items[2]), items[1], (column-length),
+                            column) and (items[0] == row)))]
+                if hd1 and not dup_list:
+                    dup_list = [items for items in hd1 if
+                        ((overlap (items[1], (items[1]+items[2]), (column-length),
                         column) and (items[0] == row)))]
-            if hd1 and not dup_list:
-                dup_list = [items for items in hd1 if
-                    ((overlap (items[1], (items[1]+items[2]), (column-length),
-                    column) and (items[0] == row)))]
 
-        elif (func == 2 and direction == 1):   # down
-            if vd1 and not dup_list:
-                dup_list = [items for items in vd1 if
-                    ((overlap (items[0], (items[0]+items[2]), row, (row+length))
-                        and (items[1] == column)))]
-            if vd2 and not dup_list:
-                dup_list = [items for items in vd2 if
-                    ((overlap ((items[0]-items[2]), items[0], row,
-                        (row+length)) and (items[1] == column)))]
+            elif (func == 2 and direction == 1):   # down
+                if vd1 and not dup_list:
+                    dup_list = [items for items in vd1 if
+                        ((overlap (items[0], (items[0]+items[2]), row, (row+length))
+                            and (items[1] == column)))]
+                if vd2 and not dup_list:
+                    dup_list = [items for items in vd2 if
+                        ((overlap ((items[0]-items[2]), items[0], row,
+                            (row+length)) and (items[1] == column)))]
 
-        elif (func == 2 and direction == 2):  # up
-            if vd2 and not dup_list:
-                dup_list = [items for items in vd2 if
-                    ((overlap ((items[0]-items[2]), items[0], (row-length), row)
-                        and (items[1] == column)))]
-            if vd1 and not dup_list:
-                dup_list = [items for items in vd1 if
-                    ((overlap (items[0], (items[0]+items[2]), (row-length),
-                        row) and (items[1] == column)))]
+            elif (func == 2 and direction == 2):  # up
+                if vd2 and not dup_list:
+                    dup_list = [items for items in vd2 if
+                        ((overlap ((items[0]-items[2]), items[0], (row-length), row)
+                            and (items[1] == column)))]
+                if vd1 and not dup_list:
+                    dup_list = [items for items in vd1 if
+                        ((overlap (items[0], (items[0]+items[2]), (row-length),
+                            row) and (items[1] == column)))]
 
-        elif (func == 3 and (direction == 1 or direction == 4)):  # up-right or
-            dup_list = [items for items in dd1 if                 # down-left
-                (((items[0]+items[1]) == (row+column)) and dd1)]
+            elif (func == 3 and (direction == 1 or direction == 4)):  # up-right or
+                dup_list = [items for items in dd1 if                 # down-left
+                    (((items[0]+items[1]) == (row+column)) and dd1)]
+                if not dup_list:
+                    dup_list = [items for items in dd4 if
+                        (((items[0]+items[1]) == (row+column)) and dd4)]
+
+            elif (func == 3 and (direction == 2 or direction == 3)):  # up-left or
+                dup_list = [items for items in dd2 if                 # down-right
+                    (((items[0]-items[1]) == (row-column)) and dd2)]
+                if not dup_list:
+                    dup_list = [items for items in dd3 if
+                        (((items[0]-items[1]) == (row-column)) and dd3)]
+
             if not dup_list:
-                dup_list = [items for items in dd4 if
-                    (((items[0]+items[1]) == (row+column)) and dd4)]
+                result = [row, column, length]
 
-        elif (func == 3 and (direction == 2 or direction == 3)):  # up-left or
-            dup_list = [items for items in dd2 if                 # down-right
-                (((items[0]-items[1]) == (row-column)) and dd2)]
-            if not dup_list:
-                dup_list = [items for items in dd3 if
-                    (((items[0]-items[1]) == (row-column)) and dd3)]
+                if (func == 1 and direction == 1):
+                    hd1.append(result)
+                elif (func == 1 and direction == 2):
+                    hd2.append(result)
+                elif (func == 2 and direction == 1):
+                    vd1.append(result)
+                elif (func == 2 and direction == 2):
+                    vd2.append(result)
+                elif (func == 3 and direction == 1):
+                    dd1.append(result)
+                elif (func == 3 and direction == 2):
+                    dd2.append(result)
+                elif (func == 3 and direction == 3):
+                    dd3.append(result)
+                elif (func == 3 and direction == 4):
+                    dd4.append(result)
 
-        if not dup_list:
-            result = [row, column, length]
+                regen_coord = 0
 
-            if (func == 1 and direction == 1):
-                hd1.append(result)
-            elif (func == 1 and direction == 2):
-                hd2.append(result)
-            elif (func == 2 and direction == 1):
-                vd1.append(result)
-            elif (func == 2 and direction == 2):
-                vd2.append(result)
-            elif (func == 3 and direction == 1):
-                dd1.append(result)
-            elif (func == 3 and direction == 2):
-                dd2.append(result)
-            elif (func == 3 and direction == 3):
-                dd3.append(result)
-            elif (func == 3 and direction == 4):
-                dd4.append(result)
+            elif dup_list:
+                # print dup_list
+                row, column, func, length, direction = coord()
+                #print "dedup"
+                del dup_list[:]
 
-            regen_coord = 0
+        regen_coord = 1
 
-        elif dup_list:
-            # print dup_list
-            row, column, func, length, direction = coord()
-            #print "dedup"
-            del dup_list[:]
+    # Now that we have the random coordinates we pass that to the routine to generate
+    # the actual number
 
-    regen_coord = 1
+        numb=find_numb(matrix,row,column,func,length,direction)
 
-# Now that we have the random coordinates we pass that to the routine to generate
-# the actual number
+    # option to print the coordinates of each number in the matrix...
 
-    numb=find_numb(matrix,row,column,func,length,direction)
+        #print ("row=%s, column=%s, func=%s, length=%s, direction=%s" % (row+1, column+1,
+        #    func, length, direction))
 
-# option to print the coordinates of each number in the matrix...
+        numbers_puzz = "".join([str(x) for x in numb])
 
-    #print ("row=%s, column=%s, func=%s, length=%s, direction=%s" % (row+1, column+1,
-    #    func, length, direction))
+    # we get the number back from the routine then append to the html file we will write
+    # at the end
 
-    numbers_puzz = "".join([str(x) for x in numb])
+        #html_content += """<DIV style="display:inline-block Right; font-family: arial; font-style: italic;
+        #    font-variant:small-caps; font-weight:bold;letter-spacing: 4px; font-size:25;
+        #    width: 250px; left: 650px; height: 25px">"""
+        html_content += """<span>"""
+        html_content += numbers_puzz
+        html_content += """</span>"""
+        html_content += """<br>"""
+    html_content += """</div>"""
+        #position_from_top += 29
 
-# we get the number back from the routine then append to the html file we will write
-# at the end
+    if number_of_puzzles > 1:
+        html_content += """
 
-    html_content += """<DIV style="position: absolute; font-family: arial; font-style: italic;
-        font-variant:small-caps; font-weight:bold;letter-spacing: 4px; font-size:25;
-        top: """ + str(position_from_top) + """px; width: 250px; left: 650px; height: 25px">"""
-    html_content += numbers_puzz
-    html_content += """</div>
-    """
-    position_from_top += 29
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <div class="pagebreak"> </div>
+
+        """
+
+        y = 0
+        matrix = []
+        #time.sleep(0.5)
+        hd1,hd2,vd1,vd2,dd1,dd2,dd3,dd4=([] for i in range(8))
+
 
 # Now complete the trailing part of the html file
 html_content += """
